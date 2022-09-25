b43438e8de3c4d1f6442a8079827518f7ce70b78
martinfs1 <martinfernando-sandander@hotmail.com>
2022-09-25 11:41:14 -0300

    building a grid

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const btnProve = document.querySelector("#btnProve");
-btnProve.addEventListener('click', wave);
 
-function wave() {
-    console.log('Hello my friend!');
+function do16x16Divs() {
+  const body = document.querySelector("body");
+  const mainDiv = document.createElement("div");
+  mainDiv.classList.add("container");
+
+  for (let index = 0; index < 16; index++) {
+   
+      for (let index = 0; index < 16; index++) {
+        const div = document.createElement("div");
+        div.classList.add(`innerDiv`);
+        mainDiv.appendChild(div);
+      }
+  }
+  body.appendChild(mainDiv);
 }
+do16x16Divs();
