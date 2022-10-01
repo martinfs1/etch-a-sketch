d578433a994547f545ab6d46c831ce5df6b275f0
martinfs1 <martinfernando-sandander@hotmail.com>
2022-10-01 20:06:18 -0300

    problem solved but I need to refactor my solution

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
 const body = document.querySelector("body");
 const mainDiv = document.createElement("div");
+mainDiv.setAttribute("id", "mainDiv");
 mainDiv.classList.add("container");
 
-const btn = document.createElement('button')
-btn.setAttribute('id', 'ask')
-btn.classList.add('askBtn')
-btn.textContent = 'Number of squares '
-let numOfSquares;
+const btn = document.createElement("button");
+btn.setAttribute("id", "ask");
+btn.classList.add("askBtn");
+btn.textContent = "Number of squares";
+let answer = 16;
+let numOfSquares = 960 / answer;
+let div;
 
-body.appendChild(btn)
-btn.addEventListener('click', e => {
-  numOfSquares = Number.parseInt(prompt('Enter the number of squares (maximum 100)'))
-  console.log(typeof numOfSquares, numOfSquares);
-  drawDivs(numOfSquares)
-});
+drawDivs(numOfSquares);
+
+body.appendChild(btn);
+body.appendChild(mainDiv);
 
 function drawDivs(num) {
-  for (let index = 0; index < numOfSquares; index++) {
-  
-    for (let j = 0; j < numOfSquares; j++) {
-      const div = document.createElement("div");
+  for (let i = 0; i < answer; i++) {
+    for (let j = 0; j < answer; j++) {
+      div = document.createElement("div");
       div.setAttribute("id", "theDiv");
       div.classList.add("innerDiv");
+      div.style.cssText = `width: ${num}px; height: ${num}px;`;
       mainDiv.appendChild(div);
     }
-    // const nextLine = document.createElement('br');
-    // mainDiv.appendChild(nextLine)
-
-
-  
   }
-  body.appendChild(mainDiv);
-
-  
 }
 
+function paint() {
+  const divReference = document.querySelectorAll("#theDiv");
+  divReference.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+      div.classList.add("paintBlack");
+    });
+  });
+}
+paint()
 
+function reset() {
+  let element = document.querySelectorAll("#theDiv");
+  element.forEach((node) => {
+    mainDiv.removeChild(node);
+  });
+}
 
-
-const divReference = document.querySelectorAll('#theDiv');
-divReference.forEach((div) => {
-    div.addEventListener('mouseenter', () => {
-        div.classList.add('paintBlack');
-    });    
+btn.addEventListener("click", (e) => {
+  answer = Number.parseInt(prompt("Enter the number of squares (maximum 100)"));
+  numOfSquares = 960 / answer;
+  reset();
+  drawDivs(numOfSquares);
+  paint()
 });
-
-
-
